9435bf3fb9718e8fb41b357220947530897db1bf
Alastor Wu <alwu@mozilla.com>
2017-09-18 18:05:21 +0800

    Bug 1400758 - part1 : report the error description with InitPromise for Apple's decoders. r=jya

--- dom/media/platforms/apple/AppleATDecoder.cpp
+++ dom/media/platforms/apple/AppleATDecoder.cpp
@@ -4,11 +4,11 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include "AppleATDecoder.h"
 #include "AppleUtils.h"
 #include "MP4Decoder.h"
 #include "Adts.h"
 #include "MediaInfo.h"
-#include "AppleATDecoder.h"
 #include "mozilla/Logging.h"
 #include "mozilla/SyncRunnable.h"
 #include "mozilla/UniquePtr.h"
@@ -56,8 +56,10 @@ RefPtr<MediaDataDecoder::InitPromise>
 AppleATDecoder::Init()
 {
   if (!mFormatID) {
-    NS_ERROR("Non recognised format");
-    return InitPromise::CreateAndReject(NS_ERROR_DOM_MEDIA_FATAL_ERR, __func__);
+    return InitPromise::CreateAndReject(
+      MediaResult(NS_ERROR_DOM_MEDIA_FATAL_ERR,
+                  RESULT_DETAIL("Non recognised format")),
+      __func__);
   }
 
   return InitPromise::CreateAndResolve(TrackType::kAudioTrack, __func__);
