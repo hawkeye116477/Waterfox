64d2a697e2957f07ced85daadf6500c62f069f19
hawkeye116477 <hawkeye116477@gmail.com>
2021-09-05 23:05:57 +0200

    Update build.sh

--- build/github-actions/build.sh
+++ build/github-actions/build.sh
@@ -1,3 +1,3 @@
 cd $GITHUB_WORKSPACE
-export PATH="/C/Program Files (x86)/Windows Kits/10/bin/10.0.17763.0/x64:/C/Rust/.cargo/bin:/C/ProgramData/scoop/shims:$PATH"
+export PATH="/C/Rust/.cargo/bin:/C/ProgramData/scoop/shims:$PATH"
 ./mach build
